35382229c286d79913564b0888caae7d78563690
KieuXuanDuong <duongkxth1908064@fpt.edu.vn>
2019-10-04 22:50:22 +0700

    submit assignment 2_1: find max, min

--- assignment2_1.cpp
+++ assignment2_1.cpp
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int main(){
+	
+	float a, b, c, max, min;
+	
+	printf("nhap vao 3 so:\n");
+	scanf("%f",&a);
+	scanf("%f",&b);
+	scanf("%f",&c);
+	
+	if (a<b){
+		max = b;
+		min = a;
+	}else{
+		max = a;
+		min = b;
+	}
+	
+	if (c<min){
+		min = c;
+	}
+	
+	if (c>max){
+		max = c;
+	}
+	
+	printf("max: %f\n", max);
+	printf("min: %f\n", min);
+
+	return 0;
+}
+
 
