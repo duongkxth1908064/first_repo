efa5b05b17344480ad95108247314fb60d28edb2
Kieu Xuan Duong <duong>
2019-10-08 22:27:54 +0700

    submit assignment 3_1

---
+++ assignment3_1.cpp
@@ -0,0 +1,22 @@
+// nhap vao 2 so, in ra so lon hon
+
+
+#include <stdio.h>
+
+int main(){
+	
+	int a, b;
+	
+	printf("nhap so thu nhat: ");
+	scanf("%d",&a);
+	printf("\nnhap so thu hai: ");
+	scanf("%d",&a);
+	printf("\nso lon hon la: ");
+	if (a>b) {
+	    printf("%d",a);
+	} else {
+		printf("%d",b);
+	}
+	
+	return 0;
+}
