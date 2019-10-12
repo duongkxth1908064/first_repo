054f41ce0fea0fb6c83ec6d5b6c8a7e07cd23bac
kieuxuanduong <duongkieu91@gmail.com>
2019-10-12 15:29:37 +0700

    bai 4_1

---
+++ assignment4_1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a,b,c;
+	do {
+	    printf("nhap a: ");
+		scanf("%d",&a);
+		printf("nhap b: ");
+		scanf("%d",&b);
+		printf("nhap c: ");
+		scanf("%d",&c);
+	} while (
+	    !(((a+b)>c ) && ((a+c)>b ) && ((c+b)>a) )
+	);
+	
+	printf("ban da nhap dung 3 canh cua 1 tam giac\n");
+	
+	return 0;
+}
